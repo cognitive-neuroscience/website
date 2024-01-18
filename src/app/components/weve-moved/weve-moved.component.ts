bef8d384f2fbb34c3debcbfcae1a612de8507c17
Nicholas Lee <niconal902@gmail.com>
2024-01-18 16:29:22 -0600

    feat: add weve moved message

---
+++ src/app/components/weve-moved/weve-moved.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weve-moved',
+  templateUrl: './weve-moved.component.html',
+  styleUrls: ['./weve-moved.component.scss']
+})
+export class WeveMovedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
